4d50e5887468bebf6e363d845b2c2d24d984e98d
Michael Sjöberg <mail@michaelsjoeberg.com>
2024-07-31 23:28:43 +0800

    Update out.c

--- out.c
+++ out.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  int x = 2;
+  int x;
+  int is_true;
+  x = 2;
   if (x > 0) {
-  int is_true = 1;
+  is_true = 1;
   } else {
-  int is_true = 0;
+  is_true = 0;
   }
   printf("%d\n", is_true);
 
