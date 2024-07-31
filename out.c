9c037e27ad59a05dc9e7407c3fcbab03a982740a
Michael Sjöberg <mail@michaelsjoeberg.com>
2024-07-31 20:40:36 +0800

    Update out.c

--- out.c
+++ out.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  int x = 2 * 2;
-  int y = x + 2;
-  if (1) {
-  printf("%d\n", y);
+  int x = 2;
+  if (x > 0) {
+  int is_true = 1;
   } else {
-  printf("%d\n", x);
+  int is_true = 0;
   }
+  printf("%d\n", is_true);
 
   return 0;
 }
